a7c8f8234252bbb18fd8a179d9f617ea764beb3f
javachohj <javachohj@sampyo.co.kr>
2025-03-18 09:30:16 +0900

    메뉴 정리
    메뉴정리~

--- front/pmsweb_repo-main/src/router/CA/router.js
+++ front/pmsweb_repo-main/src/router/CA/router.js
@@ -3,15 +3,15 @@ import CA1000_10 from '@/views/CA/CA1000_10.vue'
 
 export const caRoutes = [
   {
-    path: '/test2',
+    path: '/CA1000_20/:receivedSeq?',
     name: 'CA1000_20',
-    component: CA1000_20
+    component: CA1000_20,
+    props: true
   },  
   {  
-    path: 'ca1000_10/:receivedSeq?', // receivedSeq 파라미터를 선택적으로 받음
+    path: 'ca1000_10', // receivedSeq 파라미터를 선택적으로 받음
     name: 'CA1000_10',
-    component: CA1000_10,
-    props: true
+    component: CA1000_10,    
   },    
   
   // 다른 라우트 정의...
