ef9a1f8d4005d1f83e0a8ee09a72fc451c182ffa
javachohj <javachohj@sampyo.co.kr>
2025-03-18 10:03:44 +0900

    asd

--- front/pmsweb_repo-main/src/router/CA/router.js
+++ front/pmsweb_repo-main/src/router/CA/router.js
@@ -3,13 +3,13 @@ import CA1000_10 from '@/views/CA/CA1000_10.vue'
 
 export const caRoutes = [
   {
-    path: '/CA1000_20/:receivedSeq?',
+    path: '/CA1000_20/:receivedSeq?', // receivedSeq 파라미터를 선택적으로 받음
     name: 'CA1000_20',
     component: CA1000_20,
     props: true
   },  
   {  
-    path: 'ca1000_10', // receivedSeq 파라미터를 선택적으로 받음
+    path: 'ca1000_10', 
     name: 'CA1000_10',
     component: CA1000_10,    
   },    
