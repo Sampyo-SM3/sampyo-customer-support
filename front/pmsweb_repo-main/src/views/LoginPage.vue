d44af8d697b5c97780f54e0bcb4c04170f6dd4fc
srbyeon00 <srbyeon@sampyo.co.kr>
2025-05-14 10:55:50 +0900

    0514

--- front/pmsweb_repo-main/src/views/LoginPage.vue
+++ front/pmsweb_repo-main/src/views/LoginPage.vue
@@ -9,8 +9,10 @@
             </v-col>
             <v-col cols="12" md="5">
               <v-card-text class="text-center pa-8 pa-sm-12">
-                <h1 class="text-h3 font-weight-bold mb-2">Welcome</h1>
-                <h2 class="text-h5 font-weight-bold mb-8">sampyo cement</h2>
+                <!-- <h1 class="text-h3 font-weight-bold mb-2">Welcome</h1>
+                <h2 class="text-h5 font-weight-bold mb-8">sampyo cement</h2> -->
+                <img class="mb-8" :src="require('@/assets/SAMPYO_Cement_Signature_영문.png')" alt="Sampyo Cement Logo"
+                  style="width: 200px;" />
                 <v-form @submit.prevent="login" class="mb-8">
                   <v-text-field v-model="username" label="Account" prepend-inner-icon="mdi-account" variant="outlined"
                     class="mb-4" color="primary"></v-text-field>
@@ -18,21 +20,23 @@
                     :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
                     @click:append-inner="showPassword = !showPassword" :type="showPassword ? 'text' : 'password'"
                     variant="outlined" color="primary" class="mb-2"></v-text-field>
-                  <div class="text-right mb-4">
+                  <!-- 개발전으로 주석처리 -->
+                  <!-- <div class="text-right mb-4">
                     <v-btn variant="text" color="primary" density="compact" class="text-caption">
                       비밀번호를 잊으셨나요?
                     </v-btn>
-                  </div>
+                  </div> -->
                   <v-btn block color="primary" size="large" type="submit" class="mb-6" elevation="2">
                     로그인
                   </v-btn>
                 </v-form>
-                <div class="text-divider mb-6" style="color: #808080;">간편 로그인</div>
+                <!-- 개발 전으로 주석처리 -->
+                <!-- <div class="text-divider mb-6" style="color: #808080;">간편 로그인</div>
                 <div class="text-center">
                   <v-btn icon="mdi-google" color="surface" elevation="2" @click="googleLogin" class="rounded-circle">
                     <v-icon color="error">mdi-google</v-icon>
                   </v-btn>
-                </div>
+                </div> -->
               </v-card-text>
             </v-col>
           </v-card>
@@ -67,7 +71,6 @@ export default {
       loading: false,
       showError: false,
       errorMessages: '',
-
     }
   },
   computed: {
@@ -143,38 +146,6 @@ export default {
         this.loading = false;
       }
     },
-
-
-
-    // async login() {
-    //   this.loading = true;
-
-    //   try {        
-    //     const success = await this.authStore.login({
-    //       username: this.username,
-    //       password: this.password
-    //       // username: '1',
-    //       // password: '1' 
-    //     });        
-
-    //     if (success) {
-    //       this.$router.push({ name: 'Main' });
-    //     } else {
-    //       // 로그인 실패 (authStore에서 false 반환)
-    //       this.showError = true;
-    //       this.errorMessages = this.authStore.error || '로그인에 실패했습니다.';
-    //     }      
-    //   } catch (error) {
-    //     console.error('로그인 처리 중 오류:', error);
-    //     this.errorMessages = '로그인 처리 중 오류가 발생했습니다.';
-    //     this.showError = true;            
-    //   } finally {
-    //     this.loading = false;
-    //   }
-    // },  
-
-
-
 
     googleLogin() {
       // 여기에 Google 로그인 로직을 구현하세요
